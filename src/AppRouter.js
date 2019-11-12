79f9e7cbb0012519b9d97a59484df75dfc0908ef
luoxiapeng <949123073@qq.com>
2019-11-12 15:50:50 +0800

    嵌套路由内嵌子路由

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,6 +4,8 @@ import Index from './pages/Index'
 import List from './pages/List'
 import Home from './pages/home'
 import  './index.css'
+import Video from './video'
+import Workplace from './pages/Workplace'
 
 function AppRouter() {
   return (
@@ -22,14 +24,16 @@ function AppRouter() {
           <h3>一级导航</h3>
           <ul>
             <li><Link to="/">博客</Link></li>
-            <li><Link to="/list/">视频</Link></li>
-            <li><Link to="/home/">职场</Link></li>
+            <li><Link to="/Video/">视频</Link></li>
+            <li><Link to="/Workplace/">职场</Link></li>
           </ul>
         </div>
         <div className="rightMain">
           <Route path='/' exact component={Index}></Route>
+          <Route path='/Video/'  component={Video}></Route>
           <Route path='/list/:id' exact component={List}></Route>
           <Route path='/home/' exact component={Home}></Route>
+          <Route path='/Workplace/'  component={Workplace}></Route>
         </div>
       </div>
     </Router>
